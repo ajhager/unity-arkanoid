9ca08662fdefcaa086727e32ee1c206711b23a66
Joseph Hager <ajhager@gmail.com>
2017-05-23 14:00:19 -0500

    Add paddle controller

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+	public float speed = 150;
+
+    private new Rigidbody2D rigidbody2D;
+
+    void Awake()
+	{
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+	{
+		float direction = Input.GetAxisRaw("Horizontal");
+
+        rigidbody2D.velocity = Vector2.right * direction * speed * Time.fixedDeltaTime;
+    }
+}
