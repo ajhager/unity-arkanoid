5feb69bc4cdc500af6d6da89d82dd2666569cfa7
Joseph Hager <ajhager@gmail.com>
2017-05-23 14:18:37 -0500

    Add block controller

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
