5ac1f277bd0239ab2f39e0cf4a952a64ba36ae88
Joseph Hager <ajhager@gmail.com>
2017-05-23 14:14:07 -0500

    Add ball controller

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    public float speed = 10;
+
+    private new Rigidbody2D rigidbody2D;
+
+	void Awake()
+	{
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+	{
+        rigidbody2D.velocity = Vector2.up * speed;
+    }
+    
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player")
+        {
+			float x = (transform.position.x - other.transform.position.x) / other.collider.bounds.size.x;
+            Vector2 direction = new Vector2(x, 1).normalized;
+            rigidbody2D.velocity = direction * speed;
+        }
+    }
+}
