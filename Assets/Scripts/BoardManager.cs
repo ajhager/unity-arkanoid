2f8befb367a995356d7c695116d739a3c1f1231f
Joseph Hager <ajhager@gmail.com>
2017-05-23 13:47:22 -0500

    Reorganize game assets

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+    private string[] level;
+
+    public void SetupScene(string[] level)
+	{
+        this.level = level;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public BoardManager board;
+
+    private static string[] level1 = {
+		"rrrrrrrrrr",
+		"bbbbbbbbbb",
+		"gggggggggg",
+    };
+
+    private static string[][] levels = {
+		level1
+    };
+
+    private int levelIndex = 0;
+
+    void Awake()
+	{
+        board = GetComponent<BoardManager>();
+        InitLevel();
+    }
+	
+	void InitLevel()
+	{
+        board.SetupScene(levels[levelIndex]);
+    }
+}
